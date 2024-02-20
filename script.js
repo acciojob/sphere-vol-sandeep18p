1f36cc6d0f718c0e515386a6aae8e500d46bd682
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-20 14:42:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
-    //Write your code here
-  
+     const radius = document.getElementById("radius").value;
+    
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById("volume").value = 'NaN';
+    } else {
+        const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+        const roundedVolume = volume.toFixed(4);
+        document.getElementById("volume").value = roundedVolume;
+    }
+   return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
